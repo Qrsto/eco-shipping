723e1154cf77811150b5702f41855eb770e13e4a
DESKTOP-50NNNTU\arcad <arcano.dan@gmai.com>
2022-11-24 17:03:20 +0100

    no message

--- src/main/java/it/poliba/is45/ecoshipping/EcoshippingApplication.java
+++ src/main/java/it/poliba/is45/ecoshipping/EcoshippingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcoshippingApplication {
 
 	public static void main(String[] args) {
-		System.out.println("dffddf");
+		prova
 		SpringApplication.run(EcoshippingApplication.class, args);
 	}
 
