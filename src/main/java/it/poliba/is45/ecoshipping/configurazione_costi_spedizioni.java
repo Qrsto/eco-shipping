e1f79b0cafb2965570baa97eeef93d192f843a57
PCACER\Carlo <c.buofi@gmail.com>
2022-11-26 11:08:29 +0100

    Creata classe sulla configurazione costi spedizione

--- src/main/java/it/poliba/is45/ecoshipping/configurazione_costi_spedizioni.java
+++ src/main/java/it/poliba/is45/ecoshipping/configurazione_costi_spedizioni.java
@@ -11,6 +11,7 @@ public class configurazione_costi_spedizioni {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     
+    
     private float costo_100_grammi; // Il costo della spedizione aumenta ogni 100 grammi
     private float costo_KM; // Il costo della spedizione viene elaborato in base ai Km percorsi
     
