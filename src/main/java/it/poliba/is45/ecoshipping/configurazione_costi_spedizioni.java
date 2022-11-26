5a5efef5c9edb654535ac6ddce41a0b0e4c9a47a
PCACER\Carlo <c.buofi@gmail.com>
2022-11-26 10:44:44 +0100

    Creata classe sulla configurazione costi spedizione

---
+++ src/main/java/it/poliba/is45/ecoshipping/configurazione_costi_spedizioni.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.poliba.is45.ecoshipping;
+
+/**
+ *
+ * @author Carlo
+ */
+public class configurazione_costi_spedizioni {
+    
+}
