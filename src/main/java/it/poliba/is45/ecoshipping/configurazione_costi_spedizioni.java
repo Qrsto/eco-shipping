39ea154e94eb2caa9aeab11d943cb04bcb99dc0c
PC-DOMENICO\dcarb <d.carbonemsn00@libero.it>
2022-11-28 01:26:52 +0100

    risoluzione errori

--- src/main/java/it/poliba/is45/ecoshipping/configurazione_costi_spedizioni.java
+++ src/main/java/it/poliba/is45/ecoshipping/configurazione_costi_spedizioni.java
@@ -1,9 +1,9 @@
 package it.poliba.is45.ecoshipping;
 
-import java.persistence.Entity;
-import java.persistence.GeneratedValue;
-import java.persistence.GenerationType;
-import java.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class configurazione_costi_spedizioni {
@@ -13,23 +13,20 @@ public class configurazione_costi_spedizioni {
     
     private float costo_100_grammi; // Il costo della spedizione aumenta ogni 100 grammi
     private float costo_KM; // Il costo della spedizione viene elaborato in base ai Km percorsi
-    
-    public Float costo_100_grammi() {
-        return costo_100_grammi;
-    }
 
-    public void setcosto_100_grammi(Float costo_100_grammi) {
-        this.costo_100_grammi = costo_100_grammi;
-    }
-    
-    public Float costo_KM() {
-        return costo_KM;
-    }
-
-    public void setcosto_KM(Float costo_KM) {
+    public void setCosto_KM(float costo_KM) {
         this.costo_KM = costo_KM;
     }
 
-    
-    
+    public void setCosto_100_grammi(float costo_100_grammi) {
+        this.costo_100_grammi = costo_100_grammi;
+    }
+
+    public float getCosto_100_grammi() {
+        return costo_100_grammi;
+    }
+
+    public float getCosto_KM() {
+        return costo_KM;
+    }
 }
