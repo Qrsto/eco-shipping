dd2b15ea0d9d3c21af4684e2d44a8119ddd91cc2
PC-DOMENICO\dcarb <d.carbonemsn00@libero.it>
2022-12-09 17:01:32 +0100

    modifiche generali 2

--- src/main/java/it/poliba/is45/ecoshipping/controller/OrdineController.java
+++ src/main/java/it/poliba/is45/ecoshipping/controller/OrdineController.java
@@ -8,12 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrdineController {
-<<<<<<< HEAD
-=======
-
-
-
-    
->>>>>>> a2225e57927e74c10e6cc43975dc95b1a4f09f10
 
 }
