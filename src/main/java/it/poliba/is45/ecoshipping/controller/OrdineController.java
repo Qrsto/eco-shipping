2bf3a96721f1b982f746f0c9796fe197c0d3e901
PC-DOMENICO\dcarb <d.carbonemsn00@libero.it>
2023-01-12 00:04:01 +0100

    piccolo fix per OrdineController createOrder

--- src/main/java/it/poliba/is45/ecoshipping/controller/OrdineController.java
+++ src/main/java/it/poliba/is45/ecoshipping/controller/OrdineController.java
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RequestMapping("/api/order")
-@PreAuthorize("hasRole('ROLE_CLIENT') or hasRole('ROLE_RIDER') or hasRole('ROLE_ADMIN')")
+
 public class OrdineController {
 
 
