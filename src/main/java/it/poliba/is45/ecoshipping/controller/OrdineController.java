47a7b78234f5a4a23417adc9cb0baf318098c2fe
Danilo <arcano.dan@gmai.com>
2023-01-12 12:48:02 +0100

    no message

--- src/main/java/it/poliba/is45/ecoshipping/controller/OrdineController.java
+++ src/main/java/it/poliba/is45/ecoshipping/controller/OrdineController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials = "true")
 @RequestMapping("/api/order")
 
 public class OrdineController {
--- src/main/java/it/poliba/is45/ecoshipping/security/WebSecurityConfig.java
+++ src/main/java/it/poliba/is45/ecoshipping/security/WebSecurityConfig.java
@@ -86,8 +86,10 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     http.cors().and().csrf().disable()
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-        .antMatchers("/api/test/**").permitAll()
+        .authorizeRequests()
+            .antMatchers("/api/auth/**").permitAll()
+            .antMatchers("/api/test/**").permitAll()
+            .antMatchers("/api/order/**").permitAll()
         .anyRequest().authenticated();
     
     http.authenticationProvider(authenticationProvider());
