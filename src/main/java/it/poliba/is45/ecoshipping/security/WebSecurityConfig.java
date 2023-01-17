99e27cf718987f42de84c3b9de324c21e3b161ef
Danilo <arcano.dan@gmai.com>
2023-01-17 17:16:05 +0100

    no message

--- src/main/java/it/poliba/is45/ecoshipping/security/WebSecurityConfig.java
+++ src/main/java/it/poliba/is45/ecoshipping/security/WebSecurityConfig.java
@@ -69,17 +69,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     return new BCryptPasswordEncoder();
   }
 
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//    http.cors().and().csrf().disable()
-//      .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-//      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-//      .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-//      .antMatchers("/api/test/**").permitAll()
-//      .anyRequest().authenticated();
-//
-//    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//  }
+
   
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
